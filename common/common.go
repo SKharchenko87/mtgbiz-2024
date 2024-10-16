41d5b827c818f495e29befb35c8749e008074bba
Sergey Kharchenko <skharchenko87@yandex.ru>
2024-10-16 20:53:55 +0300

    Общий модуль

--- common/common.go
+++ common/common.go
@@ -24,3 +24,12 @@ func GetDSN() string {
 	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, port, dbname)
 	return dsn
 }
+
+func GetServerURL(postfix string) string {
+	protocol := os.Getenv("SERVER_PROTOCOL_" + postfix)
+	host := os.Getenv("SERVER_HOST_" + postfix)
+	port := os.Getenv("SERVER_PORT_" + postfix)
+	pathParam := os.Getenv("SERVER_PATH_PARAM_" + postfix)
+	serverURL := fmt.Sprintf("%s://%s:%s/%s", protocol, host, port, pathParam)
+	return serverURL
+}
